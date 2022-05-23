4bf1035ac319a2276d4a857d328e52bbdc6bbe96
sinc <73232388+kenji176@users.noreply.github.com>
2022-05-24 00:46:34 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,14 +2,19 @@ window.onload = function() {
     window.addEventListener('scroll', function(){
         const tweet = document.getElementsByClassName('css-1dbjc4n');
         for(let i = 0, len = tweet.length; i<len; i++){
-            if(tweet[i].hasAttribute("role")){
-                const role = tweet[i].getAttribute('role');
-                if(role == 'article'){
-                    const data_testid = tweet[i].getAttribute('data-testid');
-                    if(data_testid == 'tweet'){
-                        for(let n=0; n<tweet[i].childElementCount; n++) {
-                            if (tweet[i].children[n].textContent.slice( -7 ) == 'プロモーション'){
-                                tweet[i].remove();
+            if (tweet[i] == null){
+                ;
+            }else{
+                if(tweet[i].hasAttribute("role")){
+                    const role = tweet[i].getAttribute('role');
+                    if(role == 'article'){
+                        const data_testid = tweet[i].getAttribute('data-testid');
+                        if(data_testid == 'tweet'){
+                            for(let n=0; n<tweet[i].childElementCount; n++) {
+                                if (tweet[i].children[n].textContent.slice( -7 ) == 'プロモーション'){
+                                    const twi = tweet[i].parentNode.parentNode.parentNode.parentNode
+                                    twi.remove();
+                                }
                             }
                         }
                     }
